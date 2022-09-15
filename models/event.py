88056bab503b90a95bd5fda9436c64680a069a94
Cammy Smith <cjsarab1991@gmail.com>
2022-09-15 11:47:18 +0100

    Added class Event

---
+++ models/event.py
@@ -0,0 +1,9 @@
+import datetime
+
+class Event:
+    def __init__(self, date, name_of_event, no_of_guests, room_location, description):
+        self.date = date
+        self.name_of_event = name_of_event
+        self.no_of_guests = no_of_guests
+        self.room_location = room_location
+        self.description = description
