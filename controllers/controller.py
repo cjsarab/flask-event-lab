1c7ccd6a147e9d22cc1143564c79db79f717bffd
Cammy Smith <cjsarab1991@gmail.com>
2022-09-15 12:29:46 +0100

    Added labels

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,3 +3,6 @@ from app import app
 from models.event_list import events
 from models.event import Event
 
+@app.route('/events')
+def index():
+    return render_template('index.html', title='Events', sub_title='All events', events='Events')
